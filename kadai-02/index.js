2b101560bfe1537d88604fe6e0db264146cd1e0b
Inada-Ayami <inadaayami74@gmail.com>
2021-10-31 01:20:52 +0000

    課題の提出

--- kadai-02/index.js
+++ kadai-02/index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const firstName = "Ayami"
+const familyName = "Inada "
+console.log(familyName + firstName)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const dog = false
+const cat =true
+console.log(dog || cat) 
+console.log(dog && cat)　
+console.log(!dog && !cat)
+console.log(!(dog || cat))
+
+console.log(cat && !dog)
+console.log(!cat && dog)
