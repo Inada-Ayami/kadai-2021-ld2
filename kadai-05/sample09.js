637dc5ebdfbb9ce60a6cf9504b0c0b68d03fcf96
Inada-Ayami <inadaayami74@gmail.com>
2021-12-05 12:51:25 +0000

    課題を提出

--- kadai-05/sample09.js
+++ kadai-05/sample09.js
@@ -5,3 +5,19 @@ for (let index = 0; index < animals.length; index = index + 1){
     const animal = animals[index];
     console.log(`I have ${animal}.`)
 }
+
+for (const animal of animals) {
+    console.log(`I have ${animal}.`)
+}
+
+animals.forEach((animal) =>{
+    console.log(`I have ${animal}.`)
+})
+
+
+const lines = animals
+      .map((animal) => {return `I have ${animal}.`})
+      .map(line =>{return line.toUpperCase() })
+      .forEach( line => {console.log(line) })
+
+
---
+++ kadai-05/sample10.js
@@ -0,0 +1,16 @@
+const cat ={
+    name:"Tama",
+    age:2,
+    sex:"male",
+}
+
+const keys = Object.keys(cat)
+console.log(keys)
+
+const values = Object.values(cat)
+console.log(values)
+
+for(const key of keys){
+    const value = cat[key]
+    console.log(key,value)
+}
